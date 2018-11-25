efbc59e3382152a6405d6ed0955f1feb8c4527d9
Andriy Gordiychuk <a.gordiychuk@icloud.com>
2018-11-25 17:09:23 +0200

    VM initialisation fixed

--- vm_initializer.c
+++ vm_initializer.c
@@ -10,11 +10,17 @@ void	create_vm(t_vm **vm)
 	(*vm)->cycle_to_die = CYCLE_TO_DIE;
 	(*vm)->process = NULL;
 	(*vm)->player = NULL;
-	if (((*vm)->mem = ft_memalloc(MEM_SIZE * sizeof(char))) == NULL)
+	if (((*vm)->mem = ft_memalloc(MEM_SIZE * sizeof(t_map_cell *))) == NULL)
 		die("Error: Out of memory!");
 	i = 0;
 	while (i < MEM_SIZE)
-		(*vm)->mem[i++] = 0;
+	{
+		if (((*vm)->mem[i] = ft_memalloc(sizeof(t_map_cell))) == NULL)
+			die("Error: Out of memory!");
+		(*vm)->mem[i]->value = 0;
+		(*vm)->mem[i]->last_update = 0;
+		(*vm)->mem[i++]->player = NULL;
+	}
 }
 
 void	create_player(t_player **p, char *filename)
